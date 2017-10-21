0043c7fa6819b95b5255c827c3d8d0d664679d79
sanatiel90 <sanatielbarros@gmail.com>
2017-10-20 23:30:09 -0300

    testando conexao com banco

--- app.js
+++ app.js
@@ -4,33 +4,64 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var cassandra = require('cassandra-driver');
+
+app.use(bodyParser.json());
+
+var client = new cassandra.Client({contactPoints:['127.0.0.1'], keyspace:'trabspd'});
+
+
+
+client.connect(function(err){
+	console.log(err);
+});
+
 
 app.get('/contatos', function(req, res){
 
-	var c = [
+	var contatos = [
 		{
 			"nome":"michael",
 			"id":"2",
 			"tel":"88888889"
 		},
 		{
-			"nome":"michael",
-			"id":"2",
-			"tel":"88888889"
+			"nome":"kate",
+			"id":"3",
+			"tel":"84777452"
 		
 		},
 		{
-
+			"nome":"marie",
+			"id":"4",
+			"tel":"87741011"
+		
 		}
 
-	] 
+	];
+
+	res.send(JSON.stringify(contatos)); 
 });
 
+app.get('/contatozin', function(req, res){
 
-app.get('/', function(req,res){
-	res.send('Hello World');
+
+	client.execute("SELECT * FROM trabspd.contatos WHERE ID = 2", function(err, result){
+		
+				if(err){
+					res.send(err);
+				}
+
+				var user = result.rows[0];
+
+				res.json({name:user.nome});
+			
+		
+
+	});
 
 });
+
 
 app.listen(3000, function(){
 	console.log('server runnning');
